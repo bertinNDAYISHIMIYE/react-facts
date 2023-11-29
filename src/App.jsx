1228505dab47b49f782bc55e9deb3bd7e2e3368e
Bertin Ndayishimiye <neddybertin@gmail.com>
2023-11-30 00:34:47 +0100

    Code refactor add dark/lightMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import Main from './components/Main'
-import Navbar from './components/Navbar'
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 
-function App() {
-
-  return (
-    <div className="app">
-    <Navbar />
-    <Main />
-    </div>
-  )
+export default function App() {
+    const [mode, setMode] = React.useState(false)
+    function toggleDarkMode() {
+        setMode(prev => !prev)
+    }
+    return (
+        <div className="container">
+            <Navbar
+            darkMode={mode}
+            toggleDarkMode={toggleDarkMode}
+             />
+            <Main
+            darkMode={mode}
+             />
+        </div>
+    )
 }
-
-export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
+/* eslint-disable react/prop-types */
 
-export default function Navbar () {
+export default function Navbar (props) {
     return(
-        <nav>
-        <img src="src/assets/react.svg" className="nav--icon" />
-        <h3 className="nav--logo_text">ReactFacts</h3>
-        <h4 className="nav--title">React Course - Project 1</h4>
-    </nav>
+        <nav 
+            className={props.darkMode ? "dark": ""}
+        >
+            <img 
+                className="nav--logo_icon"
+                src="src/assets/react.svg"
+            />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            
+            <div 
+                className="toggler" 
+            >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
     )
 
 }
